ebc9e8f1f8939fbac6753e63b51c0a8f30f5adb3
Ethan Caballero <evc123@att.net>
2014-01-13 13:37:45 -0600

    giggidy giggidy

---
+++ groovy/groovy/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  groovy
+//
+//  Created by Ethan Caballero on 1/13/14.
+//  Copyright (c) 2014 Ethan Caballero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
